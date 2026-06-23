a3c65ce3979bd67928ff50759f905db9dc789af5
Hashimoto1a <newtondainercia@gmail.com>
2026-06-23 16:02:29 -0300

    User deletion with Delete

--- src/main/java/com/Hashimoto1a/workshopmongo/services/UserService.java
+++ src/main/java/com/Hashimoto1a/workshopmongo/services/UserService.java
@@ -14,25 +14,30 @@ import com.Hashimoto1a.workshopmongo.services.exception.ObjectNotFoundException;
 @Service
 public class UserService {
 
-	@Autowired //instanciar automaticamente o objeto aqui no servico
+	@Autowired // instanciar automaticamente o objeto aqui no servico
 	private UserRepository repo;
-	
-	public List<User> findAll(){
+
+	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
 	public User findById(String id) {
 		Optional<User> user = repo.findById(id);
-		
+
 		return user.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
 	}
-	
+
 	public User insert(User obj) {
 		return repo.insert(obj);
 	}
-	
+
+	public void delete(String id) {
+		findById(id);
+		repo.deleteById(id);
+	}
+
 	public User fromDTO(UserDTO objDTO) {
 		return new User(objDTO.getId(), objDTO.getName(), objDTO.getEmail());
 	}
-	
+
 }
