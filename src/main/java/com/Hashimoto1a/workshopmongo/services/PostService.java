66fff2700f3abf3af5ac95f30be62626275216c6
Hashimoto1a <newtondainercia@gmail.com>
2026-06-24 14:47:29 -0300

    Endpoint to get a post by ID

---
+++ src/main/java/com/Hashimoto1a/workshopmongo/services/PostService.java
@@ -0,0 +1,24 @@
+package com.Hashimoto1a.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.Hashimoto1a.workshopmongo.domain.Post;
+import com.Hashimoto1a.workshopmongo.repository.PostRepository;
+import com.Hashimoto1a.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+
+	@Autowired // instanciar automaticamente o objeto aqui no servico
+	private PostRepository repo;
+
+	public Post findById(String id) {
+		Optional<Post> post = repo.findById(id);
+
+		return post.orElseThrow(() -> new ObjectNotFoundException("Object not found"));
+	}
+
+}
