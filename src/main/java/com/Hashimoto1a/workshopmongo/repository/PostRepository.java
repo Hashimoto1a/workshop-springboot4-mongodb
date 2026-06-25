1a769a89333523532106ec9bc2fbc4271e0af9e5
Hashimoto1a <newtondainercia@gmail.com>
2026-06-25 09:21:09 -0300

    simple query using query methods

--- src/main/java/com/Hashimoto1a/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/Hashimoto1a/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.Hashimoto1a.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,4 +10,5 @@ import com.Hashimoto1a.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{ //<tipo da classe de dominio a ser gerenciado, tipo de id(no caso o id usado é string)>
 	
+	List<Post> findByTitleContainingIgnoreCase(String text);
 }
--- src/main/java/com/Hashimoto1a/workshopmongo/resources/PostResource.java
+++ src/main/java/com/Hashimoto1a/workshopmongo/resources/PostResource.java
@@ -1,22 +1,17 @@
 package com.Hashimoto1a.workshopmongo.resources;
 
-import java.net.URI;
 import java.util.List;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.Hashimoto1a.workshopmongo.domain.Post;
-import com.Hashimoto1a.workshopmongo.domain.User;
-import com.Hashimoto1a.workshopmongo.dto.UserDTO;
+import com.Hashimoto1a.workshopmongo.resources.util.URL;
 import com.Hashimoto1a.workshopmongo.services.PostService;
 
 @RestController
@@ -32,5 +27,12 @@ public class PostResource {
 		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
+	
+	@RequestMapping(value = "/titlesearch", method = RequestMethod.GET)
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(value = "text", defaultValue = "") String text){
+		text = URL.decodeParam(text);
+		List<Post> list = service.findyByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}	
 	
 }
